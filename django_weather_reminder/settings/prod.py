dcb72d8c86e9a1631ff2d898b4711994f92cc5ee
Serhii Sokhatskyi <sokhatsky.98@gmail.com>
2023-09-07 12:39:14 +0300

    Prod settings changes.

--- django_weather_reminder/settings/prod.py
+++ django_weather_reminder/settings/prod.py
@@ -1,26 +1,24 @@
-import os
-
 from .base import *
 
-DEBUG = False
+DEBUG = env.str('DEBUG') in ['1', 'true']
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DBNAME'),
-        'HOST': os.environ.get('DBHOST'),
-        'USER': os.environ.get('DBUSER'),
-        'PASSWORD': os.environ.get('DBPASS'),
+        'NAME': env.str('DBNAME'),
+        'HOST': env.str('DBHOST'),
+        'USER': env.str('DBUSER'),
+        'PASSWORD': env.str('DBPASS'),
         'OPTIONS': {'sslmode': 'require'},
     }
 }
 
 # REDIS related settings
-CELERY_BROKER_URL = os.environ.get('AZURE_REDIS_CONNECTIONSTRING'),
+CELERY_BROKER_URL = env.str('AZURE_REDIS_CONNECTIONSTRING'),
 CELERY_BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
-CELERY_RESULT_BACKEND = os.environ.get('AZURE_REDIS_CONNECTIONSTRING'),
+CELERY_RESULT_BACKEND = env.str('AZURE_REDIS_CONNECTIONSTRING'),
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
