880e69f8ea06184857fdca7b3db4d752f66c0093
Serhii Sokhatskyi <sokhatsky.98@gmail.com>
2023-09-07 12:55:06 +0300

    Base dir correction.

--- django_weather_reminder/settings/base.py
+++ django_weather_reminder/settings/base.py
@@ -18,7 +18,7 @@ env = environ.Env()
 environ.Env.read_env()  # reading .env file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
