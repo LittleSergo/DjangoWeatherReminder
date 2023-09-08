cd9683e0648d80eb68bed15e272d8b520bc6ea70
Serhii Sokhatskyi <sokhatsky.98@gmail.com>
2023-09-08 11:43:12 +0300

    Corrections in startup.sh.

--- startup.sh
+++ startup.sh
@@ -1,3 +1,1 @@
-export LANG=C.UTF-8
-
-gunicorn --bind=0.0.0.0 --timeout 600 django_weather_reminder.wsgi & celery -A django_weather_reminder worker -l INFO -B
+celery -A django_weather_reminder worker -l INFO -B & gunicorn --bind=0.0.0.0 --timeout 600 django_weather_reminder.wsgi
