6b76fcd556eeddb2168f925790363fdb07927c85
Serhii Sokhatskyi <sokhatsky.98@gmail.com>
2023-09-08 13:13:54 +0300

    New corrections in startup.sh.

--- startup.sh
+++ startup.sh
@@ -1,1 +1,1 @@
-celery -A django_weather_reminder worker -l INFO -B & gunicorn --bind=0.0.0.0 --timeout 600 django_weather_reminder.wsgi
+celery -A django_weather_reminder worker -l INFO -P solo & celery -A django_weather_reminder beat -l INFO & gunicorn --bind=0.0.0.0 --timeout 600 django_weather_reminder.wsgi
